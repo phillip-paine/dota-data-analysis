85d9c9ea19cc71c40b26acc6ebe89ed2c41aca1e
Phillip Paine <pmxpp88@gmail.com>
2023-01-08 12:41:43 -0800

    scraper
    write main scraper to get public match data

---
+++ data-scraper/main_data_scraper.py
@@ -0,0 +1,19 @@
+import pandas as pd
+import datetime
+
+from api_scraper import public_matches
+
+
+def run_main(publicmatches: bool):
+    """run scraper functions"""
+    if publicmatches:
+        filename = 'data/public_match_data'
+        time_suffix = datetime.datetime.now().strftime('%Y%m%d_%H%M%S')
+        public_matches('_'.join([filename, time_suffix]))
+
+    return None
+
+
+if __name__ == '__main__':
+    # scrape public match data from api and write to json:
+    run_main(publicmatches=True)
