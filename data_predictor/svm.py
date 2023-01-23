42e1e8ff89fd2751e9a5b64ae25f0ea9bb846691
Phillip Paine <pmxpp88@gmail.com>
2023-01-23 00:11:55 -0800

    data prediction
    add abstract class for models and structure to folder

---
+++ data_predictor/svm.py
@@ -0,0 +1,21 @@
+import pandas as pd
+import numpy as np
+from sklearn.svm import SVC  # support vector machine classifier
+from data_predictor.models import models
+
+
+# implement support vector machine model through the abstract model class
+class SupportVectorMachine(models):
+    def __init__(self):  # can implement here to overwrite the abstract class definition
+        self. =
+
+    def fit(self):
+
+
+    def predict(self):
+
+
+    def validation(self):
+
+
+
---
+++ data_predictor/utils.py
@@ -0,0 +1,12 @@
+import pandas as pd
+import numpy as np
+
+def create_model_features(df: pd.DataFrame):
+
+
+    return df
+
+
+
+if __name__ == '__main__':
+
