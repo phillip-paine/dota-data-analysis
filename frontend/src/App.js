f4dc424e42f044b98bbc2e438713cffe4cabe118
Phillip Paine <pmxpp88@gmail.com>
2023-06-07 00:01:41 -0700

    side by side
    add two react select boxes side by side

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,5 @@
 import './styles/App.css';
+import './styles/DropdownSelect.css';
 import { useState } from 'react';
 import DropdownSelect from './components/DropdownSelect';
 
@@ -9,10 +10,17 @@ const App = () => {
         {value: 'vanilla', label: "Vanilla"}
     ]
 
+    /* we don't want to be able to select hero more than once, so once selected in a dropdown
+    we need to remove it from future dropdown selections */
+
+
     return (
         <div className="App">
             <h1>Welcome</h1>
-            <DropdownSelect options={options} placeholder="Select a hero" />
+            <div className="DropdownRow">
+                <DropdownSelect options={options} placeholder="Select a hero" />
+                <DropdownSelect options={options} placeholder="Select second hero" />
+            </div>
         </div>
     );
 }
